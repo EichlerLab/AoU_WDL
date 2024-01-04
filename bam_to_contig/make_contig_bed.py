269c23cb9667531dcf55a615cad5ba2287f5c176
Julie Wertz <jwertz1@uw.edu>
2024-01-04 11:44:15 -0800

    Allow contig pos/ end to match within a range of ref.
    Instead of requiring exact match

--- bam_to_contig/make_contig_bed.py
+++ bam_to_contig/make_contig_bed.py
@@ -4,6 +4,7 @@ import pysam
 
 in_bam = sys.argv[1]
 gene_region_bed = sys.argv[2]
+ref_pos_end_buffer = int(sys.argv[3])  # find closest matching contig start/ end coord to reference coord, within this buffer
 
 with open(gene_region_bed) as f:
     chrom, ref_pos, ref_end = f.readline().strip().split('\t')
@@ -15,16 +16,22 @@ for r in s.fetch(reference=chrom, start=ref_pos, end=ref_end):
     is_rev_strand = r.flag & 0x10 != 0
     ctg_start = None
     ctg_end = None
+    start_min_dist = ref_pos_end_buffer
+    end_min_dist = ref_pos_end_buffer
+
     for ctg_coord, ref_coord in r.get_aligned_pairs():
         if ref_coord is None:
             continue
         # assume sorted
-        if ref_coord == ref_pos:
-            ctg_start = ctg_coord
-        elif ref_coord == ref_end:
+        if abs(ref_coord - ref_pos) <= start_min_dist:
+            ctg_start = ctg_coord  # closest position to the start position in the bed, within buffer
+            start_min_dist = abs(ref_coord - ref_pos)
+        elif abs(ref_coord - ref_end) <= end_min_dist:
             ctg_end = ctg_coord
-    assert ctg_start is not None and ctg_end is not None
-    if is_rev_strand:
-        print(f"{r.query_name}/rc\t{r.query_length - ctg_end}\t{r.query_length - ctg_start}\n")
-    else:
-        print(f"{r.query_name}\t{ctg_start}\t{ctg_end}\n")
+            end_min_dist = abs(ref_coord - ref_end)
+
+    if ctg_start is not None and ctg_end is not None:
+        if is_rev_strand:
+            print(f"{r.query_name}/rc\t{r.query_length - ctg_end - 1}\t{r.query_length - ctg_start}\n")
+        else:
+            print(f"{r.query_name}\t{ctg_start}\t{ctg_end}\n")
